7fcbfd1e7131436b2c0e30dacd9401516987f3a6
Unknown <tejas@webmavens.in>
2018-11-19 18:32:43 +0530

    db config

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
 //Database details 
 define ('DB_HOST', 'localhost'); 
 //Username 
-define ('DB_USERNAME', 'succe_encode'); 
+define ('DB_USERNAME', 'succsuccessess4u_admin'); 
 //Pass 
-define ('DB_PASS', 'Souvoroy@1'); 
+define ('DB_PASS', 'b~mYpq(n{Bu0'); 
 //Database Name 
-define ('DB_NAME', 'succe_encode'); 
+define ('DB_NAME', 'succsuccessess4u_admin'); 
 //Base URL 
-define ('BASE_URL', 'http://www.success4u.in'); 
+define ('BASE_URL', 'http://23.89.193.244/~success4u/'); 
 ?>
