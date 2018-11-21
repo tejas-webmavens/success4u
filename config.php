991cccd8bd5d31338fca08fc3953e96a900f4ae8
Unknown <tejas@webmavens.in>
2018-11-21 16:24:18 +0530

    config updated

--- config.php
+++ config.php
@@ -4,9 +4,9 @@ define ('DB_HOST', 'localhost');
 //Username 
 define ('DB_USERNAME', 'succsuccessess4u_admin'); 
 //Pass 
-define ('DB_PASS', 'b~mYpq(n{Bu0'); 
+define ('DB_PASS', 'eoh}O{s(VQ~('); 
 //Database Name 
 define ('DB_NAME', 'succsuccessess4u_admin'); 
 //Base URL 
-define ('BASE_URL', 'http://23.89.193.244/~success4u/'); 
+define ('BASE_URL', 'http://23.89.193.244/~succsuccessess4u/'); 
 ?>
